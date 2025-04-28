4986d652d0206cdb3389ad53f65148d8a76e345c
mike mwanzi <mikeabere@gmail.com>
2025-04-28 23:55:39 +0300

    moongoode DB conn update

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const app = express();
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
   
